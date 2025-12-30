675377a40b125acd45c96f149b82f984fc2777fe
tonto <tnt.kosen2423@gmail.com>
2025-12-31 03:56:47 +0900

    ransac_icpパッケージの初期設定ファイルを追加し、基本的な構成を整備

---
+++ src/ransac_icp/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+package_name = 'ransac_icp'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='tonto',
+    maintainer_email='tnt.kosen2423@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'ransac_icp_node = ransac_icp.ransac_icp_node:main'
+        ],
+    },
+)
